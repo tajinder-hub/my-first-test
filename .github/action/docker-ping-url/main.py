8dc38c064736cf802b6a4fab0a22c2a1c8c31e0b
tajinder-hub <hs9855457785@gmail.com>
2025-03-11 05:56:36 +0000

    first commit

--- .github/action/docker-ping-url/main.py
+++ .github/action/docker-ping-url/main.py
@@ -1,4 +1,11 @@
+import os
+
 def run ():
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+    website_reachable = ping_url(website_url, delay, max_trials)
     print("Hello World !!")
 
 
